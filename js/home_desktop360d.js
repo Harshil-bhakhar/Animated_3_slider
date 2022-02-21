c78f2787a5e2bf277dac1dfd1f89ce0846fde2fb
harshilbhakhar <harshil.b@hashtechy.com>
2022-02-21 14:19:23 +0530

    Changes

--- js/home_desktop360d.js
+++ js/home_desktop360d.js
@@ -379,11 +379,15 @@ if (!YT.loading) {
     YT.loading = 1;
     (function() {
         var l = [];
-        YT.ready = function(f) { if (YT.loaded) f();
-            else l.push(f) };
+        YT.ready = function(f) {
+            if (YT.loaded) f();
+            else l.push(f)
+        };
         window.onYTReady = function() { YT.loaded = 1; for (var i = 0; i < l.length; i++) try { l[i]() } catch (e) {} };
-        YT.setConfig = function(c) { for (var k in c)
-                if (c.hasOwnProperty(k)) YTConfig[k] = c[k] };
+        YT.setConfig = function(c) {
+            for (var k in c)
+                if (c.hasOwnProperty(k)) YTConfig[k] = c[k]
+        };
         var a = document.createElement("script");
         a.type = "text/javascript";
         a.id = "www-widgetapi-script";
